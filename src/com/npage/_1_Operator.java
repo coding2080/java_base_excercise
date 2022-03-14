499896c70b10c9f7f6ae4652a68d6b9b8fd635e1
coding2080 <2354taeng@gmail.com>
2022-03-14 13:45:30 +0900

    add prints operation functions

--- src/com/npage/_1_Operator.java
+++ src/com/npage/_1_Operator.java
@@ -4,6 +4,11 @@ public class _1_Operator {
 
     public static void main(String[] args) {
         System.out.println("Operation Exercise");
+
+        System.out.println(add(1, 2)); // 3
+        System.out.println(sub(10, 2)); // 8
+        System.out.println(mul(2, 2)); // 4
+        System.out.println(div(6, 3)); // 2
     }
 
     public static int add(int x, int y) {
@@ -21,6 +26,4 @@ public class _1_Operator {
     public static int div(int x, int y) {
         return 0;
     }
-
-    //나머지 연산자 함수
 }
